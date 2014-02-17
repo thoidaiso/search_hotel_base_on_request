718dda9cf1be40a8afe75315f845d111e4265ad1
sepdau <giaosudau@gmail.com>
2014-02-17 21:13:35 +0700

    For local

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -22,8 +22,8 @@ DOWNLOAD_DELAY = 1
 DUPEFILTER_CLASS = 'scrapy.dupefilter.BaseDupeFilter'
 
 import sys
-# sys.path.append('../../search_hotel_base_on_request')
-sys.path.append('/home/luan/Django_workspace/search_hotel_base_on_request')
+sys.path.append('../../search_hotel_base_on_request')
+# sys.path.append('/home/luan/Django_workspace/search_hotel_base_on_request')
 
 # DJANGO SETTING
 import os
